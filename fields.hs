c125db9bcdc894cae25b9432101711a6b0d15ec9
Johan Holmquist <holmisen@gmail.com>
2013-05-17 12:35:55 +0200

    initial

---
+++ fields.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+-- Arrange tabular data in columns.
+--
+-- Input data is pipe separated fields read from stdin.
+
+
+import Data.List   (intersperse, transpose)
+import Text.Printf (printf)
+import Text.Regex  (splitRegex, mkRegex)
+
+
+splitColumns = splitRegex (mkRegex "\\|")
+
+rowsToColumns = transpose
+columnsToRows = transpose
+
+
+formatColumns cs = map (uncurry formatColumn) (zip sizes cs)  where
+    sizes = map columnSize cs
+
+formatColumn n xs = map (formatCell n) xs
+
+formatCell = printf "%-*s"
+
+columnSize = maximum . map length
+
+mkLine = concat . intersperse "  "
+
+
+main = do
+  ls <- lines `fmap` getContents
+  let rs  = map splitColumns ls
+  let rs' = columnsToRows $ formatColumns $ rowsToColumns rs
+  let ls' = map mkLine rs'
+  mapM_ putStrLn ls'
